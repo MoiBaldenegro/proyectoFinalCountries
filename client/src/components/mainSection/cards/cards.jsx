2b763946cf14969af013904015cdcc1cfc829e81
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-08-04 11:40:00 +0200

    ultima actualizacion

--- client/src/components/mainSection/cards/cards.jsx
+++ client/src/components/mainSection/cards/cards.jsx
@@ -5,9 +5,7 @@ import Card from "./card/card";
 //////// STYLES /////////////////////
 import style from "./cards.module.css";
 
-
-
-
+// ultima version
 export default function Cards(allCountries){
     const countriesList = allCountries;
     return(
